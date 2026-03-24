0fade6416d8c07383dedbd417f809c479c283444
Ishani180 <iar30@scarletmail.rutgers.edu>
2026-03-23 22:38:36 -0400

    size

--- src/main/java/org/example/project_3/Main.java
+++ src/main/java/org/example/project_3/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
         stage.setTitle("Registration Management System");
         stage.setScene(scene);
         stage.show();
